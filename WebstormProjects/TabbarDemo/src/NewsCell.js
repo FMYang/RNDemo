3e1de778903e76b13b1c855ac8ae8b595ae4150a
yfm <yangfangming@tuandai.com>
2018-05-30 19:07:00 +0800

    -

--- WebstormProjects/TabbarDemo/src/NewsCell.js
+++ WebstormProjects/TabbarDemo/src/NewsCell.js
@@ -28,7 +28,7 @@ class NewsCell extends Component<Props> {
             let imageList = Object.values(content.image_list)
             let images = imageList.length
 
-            if (images > 0 && images < 3) {
+            if (images > 0 && images < 3) { // 一张图
                 let url = imageList[0].url_list[0].url.replace('webp', 'jpg')
                 return(
                     <View style={{flexDirection: 'row'}}>
@@ -38,8 +38,7 @@ class NewsCell extends Component<Props> {
                         </TouchableOpacity>
                     </View>
                 )
-            } else if (images >= 3) {
-                console.log("三张图")
+            } else if (images >= 3) { // 三张图
                 let url1 = imageList[0].url_list[0].url.replace('webp', 'jpg')
                 let url2 = imageList[1].url_list[1].url.replace('webp', 'jpg')
                 let url3 = imageList[2].url_list[2].url.replace('webp', 'jpg')
@@ -47,25 +46,20 @@ class NewsCell extends Component<Props> {
                     <TouchableOpacity onPress = {() => this.props.onPress(content)}>
                         <Text style={styles.cell}> {content.title} </Text>
                         <View style={{flexDirection: 'row'}}>
-                            {/*<Image style={styles.image} source={{uri: imageList[0].url_list[0].url}}/>*/}
-                            {/*'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000' +*/}
-                            {/*'&sec=1526988631291&di=9da54391aebed69610a98b4491db4dea' +*/}
-                            {/*'&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F15%2F23%2F09%2F74T58PICZjg_1024.jpg'*/}
                             <Image style={styles.image} source={{uri: url1}}/>
                             <Image style={styles.image} source={{uri: url2}}/>
                             <Image style={styles.image} source={{uri: url3}}/>
                         </View>
                     </TouchableOpacity>
                 )
-            } else {
-                console.log("无图")
+            } else { // 无图
                 return(
                     <TouchableOpacity onPress = {() => this.props.onPress(content)}>
                         <Text style={styles.cell}> {content.title} </Text>
                     </TouchableOpacity>
                 )
             }
-        } else {
+        } else { // 无图
             return(
                 <TouchableOpacity onPress = {() => this.props.onPress(content)}>
                     <Text style={styles.cell}> {content.title} </Text>
